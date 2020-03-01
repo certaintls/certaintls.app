d89848162ff66f69900569960e39ea238e9da3ce
CertainTLS <certaintls@gmail.com>
2020-03-02 00:33:54 +0800

    Implemented MacOS cert finder.

--- lib/macos_certificate_finder.dart
+++ lib/macos_certificate_finder.dart
@@ -4,13 +4,24 @@ import 'package:certaintls/certificate_finder.dart';
 
 class MacOSCertificateFinder implements CertificateFinder {
   static String systemTrustedCertsPath = '/System/Library/Keychains/SystemRootCertificates.keychain';
+  static String delimiter = '-----END CERTIFICATE-----\n';
 
   @override
   List<X509CertificateData> getSystemRootCerts() {
+    List<String> pems;
+    List<X509CertificateData> certs = [];
     Process.run('security', ['find-certificate', '-pa', systemTrustedCertsPath]).then((ProcessResult results) {
       print(results.stdout);
+      String output = results.stdout as String;
+      pems = output.split(delimiter);
     });
-    return null;
+
+    pems.forEach((pem) {
+      X509CertificateData data = X509Utils.x509CertificateFromPem(pem + delimiter);
+      certs.add(data);
+    });
+
+    return certs;
   }
 
   @override
