3a834bbb756d8f28ff3d1b9ea557ff38994622da
CertainTLS <certaintls@gmail.com>
2020-03-01 23:36:59 +0800

    Added macos system root cert path.

--- lib/macos_certificate_finder.dart
+++ lib/macos_certificate_finder.dart
@@ -3,9 +3,11 @@ import 'package:basic_utils/basic_utils.dart';
 import 'package:certaintls/certificate_finder.dart';
 
 class MacOSCertificateFinder implements CertificateFinder {
+  static String systemTrustedCertsPath = '/System/Library/Keychains/SystemRootCertificates.keychain';
+
   @override
   List<X509CertificateData> getSystemRootCerts() {
-    Process.run('security', ['find-certificate', '-pa']).then((ProcessResult results) {
+    Process.run('security', ['find-certificate', '-pa', systemTrustedCertsPath]).then((ProcessResult results) {
       print(results.stdout);
     });
     return null;
