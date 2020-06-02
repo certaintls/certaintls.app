83d67340185156ad3cf69d4e8a3cbf8570c19b39
CertainTLS <certaintls@gmail.com>
2020-06-02 11:11:31 +0800

    Debug github secret.

--- test/windows_check_certificates.dart
+++ test/windows_check_certificates.dart
@@ -16,6 +16,7 @@ void main() async {
   if (baseUrl != null) {
     uploadToDrupal = true;
   }
+  print('Revealing a test secret: ' + String.fromEnvironment('TEST_SECRET'));
   final authorizationEndpoint = Uri.parse(baseUrl + drupalEndpoints['oauth2_token']);
   final identifier = String.fromEnvironment('WIN_OAUTH2_ID', defaultValue: DotEnv().env['WIN_OAUTH2_ID']);
   final secret = String.fromEnvironment('WIN_OAUTH2_SECRET', defaultValue: DotEnv().env['WIN_OAUTH2_SECRET']);
